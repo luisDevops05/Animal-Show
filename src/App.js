5465e908e0cda36791989f8c554ae2242ce82762
JlSanchez05 <luisdevops0@gmail.com>
2023-01-15 21:35:42 -0500

    added functionality and styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import {useState} from 'react'
+import AnimalShow from './AnimalShow'
 import './style.css'
+import './App.css'
+import './AnimalShow.css'
+
+
+function getRandomAnimal(){
+    const animals  = ['bird','cat','dog','gator','horse']
+
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+
 
 function App(){
     
-    const [count,setCount] = useState(0)
+    const [animals,setAnimals] = useState([])
 
 
     const handleClick  = () => {
-        setCount(count +1)
+       setAnimals([...animals,getRandomAnimal()])
     }
+    const renderedAnimals = animals.map((animal, index) =>{
+        return <AnimalShow type={animal} key={index}/>
+    })
 
     return(
-        <div>
+        <div className='app'>
             <button onClick={handleClick}>Add Animal</button>
-            <div>
-                Number of animals: {count}
-            </div>
+            <div className='animal-list'>{renderedAnimals}</div>
         </div>
     )
 }
